e0464bc457378f55a15b89a2f5b7aa37c4f49422
Rohan <rohan.kamath.24@gmail.com>
2025-09-26 23:25:23 +0530

    Completed vectors

--- 1 Verilog Language/Modules Hierarchy/3_modules.v
+++
@@ -1,6 +0,0 @@
-module top_module ( input clk, input d, output q );
-    wire w1, w2;
-    my_dff dff1(.d(d), .clk(clk), .q(w1));
-    my_dff dff2(.d(w1), .clk(clk), .q(w2));
-    my_dff dff3(.d(w2), .clk(clk), .q(q));
-endmodule
